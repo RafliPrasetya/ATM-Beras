2a6ec2b926504e56853d66d6f605d2069af2708e
RafliPrasetya <rafliprasetya07@gmail.com>
2026-07-03 23:39:19 +0700

    feat: implement login page styling and base application layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,8 @@
 <html>
 
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ATM Beras</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/login.css'])
