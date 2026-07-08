656bef7e2dcf57e0deed1a0c907963258251baf1
RafliPrasetya <rafliprasetya07@gmail.com>
2026-07-08 23:39:21 +0700

    chore: update build assets and manifest following vite configuration changes

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    // server: {
-    //     host: '192.168.1.24',
-    // },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/landing.css',
+                'resources/css/dashboard.css',
+                'resources/css/login.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
